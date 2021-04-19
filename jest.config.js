5df27d9b918f4756f22f3c7c73b776d2d913feda
Maneren <49210777+Maneren@users.noreply.github.com>
2021-04-19 20:08:06 +0200

    added jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  verbose: true,
+  testEnvironment: 'jest-environment-node',
+  transform: {},
+  globals: {
+    extensionsToTreatAsEsm: ['.js']
+  }
+};
