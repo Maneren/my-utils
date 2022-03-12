58a4df2f01cab9785f16dbac1ced76af09b40bd7
Maneren <49210777+Maneren@users.noreply.github.com>
2022-03-12 23:34:44 +0100

    move range inside iterator.ts

--- src/iterator.ts
+++ src/iterator.ts
@@ -10,6 +10,46 @@ export function repeat<T> (value: T): Iter<T> {
   return iter(generator());
 }
 
+type R = Iter<number>;
+/**
+ * returns generator, which yields numbers from min to max with defined step
+ * if only min is specified, it yields numbers from 0 to min
+ * @param {number} start lower bound (inclusive)
+ * @param {number} end upper bound (exclusive)
+ * @param {number} step
+ * @returns {R}
+ */
+export function range (end: number): R;
+export function range (start: number, end: number): R;
+export function range (start: number, end: number, step: number): R;
+export function range (start: number, end?: number, step?: number): R {
+  function * generator (): Iterable<number> {
+    if (end === undefined) {
+      // if only start is specified, then go from 0 to "start"
+      end = start;
+      start = 0;
+    }
+
+    if (step === undefined) step = 1;
+
+    if (step < 0) {
+      if (start < end) {
+        throw new Error(
+          'when step is lower than 0, start must be larger than end'
+        );
+      }
+
+      for (let i = start; i > end; i += step) yield i;
+    } else {
+      if (start > end) throw new Error('start must be smaller than end');
+
+      for (let i = start; i < end; i += step) yield i;
+    }
+  };
+
+  return iter(generator());
+}
+
 type Predicate<T> = (value: T) => boolean;
 
 export class Iter<T> implements Iterable<T> {
